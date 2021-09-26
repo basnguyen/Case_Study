f910646afb241323d4ab44254f0ac0630f5ff785
basnguyen <basnguyen@gmail.com>
2021-09-26 15:05:23 +0200

    Cleaning of file formats, direct Loader_Worldbank to Dev_ Database

--- ESG_Country_Score/Loader_Worldbank.py
+++ ESG_Country_Score/Loader_Worldbank.py
@@ -10,73 +10,79 @@ import pandas as pd
 import sqlite3
 
 # Get ISO mapping
-ISO_mapping=pd.read_csv("Worldbank_CountryCode_to_ISO.csv",sep='\t')
-# Get list indicators and clean the name 
-WBFields=pd.read_csv("WBFields_Loader.csv",sep='\t')
+ISO_mapping = pd.read_csv("Worldbank_CountryCode_to_ISO.csv", sep="\t")
+# Get list indicators and clean the name
+WBFields = pd.read_csv("WBFields_Loader.csv", sep="\t")
 # Get list of countries to ignore
-ignore_list=pd.read_csv("Ignorelist_Worldbank.ini",sep="\t")
-ignore_list=ignore_list[ignore_list.columns[0]]
+ignore_list = pd.read_csv("Ignorelist_Worldbank.ini", sep="\t")
+ignore_list = ignore_list[ignore_list.columns[0]]
 
-WBFields["id_renamed"]=WBFields["id"].replace('\.','_', regex=True)
-WBFields.set_index("id",inplace=True)
-indicators_tofetch=WBFields["id_renamed"].to_dict()
+WBFields["id_renamed"] = WBFields["id"].replace("\.", "_", regex=True)
+WBFields.set_index("id", inplace=True)
+indicators_tofetch = WBFields["id_renamed"].to_dict()
 
 for key_i in indicators_tofetch:
     print(key_i)
-    id_i=indicators_tofetch[key_i]
-    
+    id_i = indicators_tofetch[key_i]
+
     try:
         # Fetch Data with wbdata and convert country names to ISO codescountries = [i['id'] for i in wbdata.get_country()]
-        WBDataframe = wbdata.get_dataframe({key_i : id_i}, convert_date=False).rename(index=ISO_mapping.set_index("Country Name").to_dict()['Country Code'])
-        
+        WBDataframe = wbdata.get_dataframe({key_i: id_i}, convert_date=False).rename(
+            index=ISO_mapping.set_index("Country Name").to_dict()["Country Code"]
+        )
+
         # Check that all country names are correctly mapped to an ISO code, loader will fail if it is not empty
-        list_of_masters=pd.Series(WBDataframe.index.get_level_values(0),).drop_duplicates(keep='first')
-        list_non_ISO=list_of_masters[(list_of_masters.apply(lambda x: len(x)>3)) | (list_of_masters.str.contains(r'[a-z]'))]
-        errors_to_treat=list_non_ISO[~list_non_ISO.isin(ignore_list)]
-        
-        if len(errors_to_treat)>0:
+        list_of_masters = pd.Series(
+            WBDataframe.index.get_level_values(0),
+        ).drop_duplicates(keep="first")
+        list_non_ISO = list_of_masters[
+            (list_of_masters.apply(lambda x: len(x) > 3))
+            | (list_of_masters.str.contains(r"[a-z]"))
+        ]
+        errors_to_treat = list_non_ISO[~list_non_ISO.isin(ignore_list)]
+
+        if len(errors_to_treat) > 0:
             print(errors_to_treat)
             # raise Exception("Some masters are not in ISO format, control andadd them to mapping or ignore file")
-        
+
         # remove all non ISO country
-        WBDataframe.drop(list_non_ISO.tolist(),level=0,axis=0,inplace=True)
+        WBDataframe.drop(list_non_ISO.tolist(), level=0, axis=0, inplace=True)
     except:
         print(f"Issue with loading {id_i} using API")
 
-    
     try:
         # Load into SQL Database
-        con=sqlite3.connect("../SQL_Database/ESG_Database.sqlite3")
+        con = sqlite3.connect("../SQL_Database/ESG_Database_DEV.sqlite3")
         cur = con.cursor()
-        
-        to_save=WBDataframe[id_i].rename("value")
-        to_save.to_sql(f"{id_i}",con,if_exists='replace')
-        
+
+        to_save = WBDataframe[id_i].rename("value")
+        to_save.to_sql(f"{id_i}", con, if_exists="replace")
+
         # Create view with the latest data available
-        
+
         cur.execute(f"""DROP VIEW IF EXISTS {id_i}_LATEST""")
-        cur.execute(f"""
+        cur.execute(
+            f"""
             CREATE VIEW IF NOT EXISTS {id_i}_LATEST AS
             SELECT * FROM {id_i} WHERE (country,date) in
-            (SELECT country, max(date) FROM {id_i} where value IS NOT NULL GROUP BY country)
-            """
+            (
+                SELECT country, max(date) FROM {id_i} 
+                WHERE value IS NOT NULL GROUP BY country
             )
-             # Create view with the latest data available and normalized
+            """
+        )
+        # Create view with the latest data available and normalized
         cur.execute(f"""DROP VIEW IF EXISTS {id_i}_RENORMALIZED""")
         cur.execute(f"""
             CREATE VIEW IF NOT EXISTS {id_i}_RENORMALIZED AS
-        SELECT 
-            country,
-            date,
-            100*(value - min_value) / (max_value -min_value) normalized_value
-        FROM {id_i}_LATEST  
-        CROSS JOIN (
+            SELECT country, date, 100*(value - min_value) / (max_value -min_value) normalized_value
+            FROM {id_i}_LATEST  
+            CROSS JOIN (
             SELECT min(value) min_value, max(value) max_value
             FROM {id_i}_LATEST
-        ) 
+            ) 
             """
         )
         cur.close()
     except:
         print(f"Issue with {id_i}, could not create view")
-    
